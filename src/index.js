21717bc626ecd9d4fa5072be426c982641f336f7
migu <migu@fanhaoyue.com>
2019-08-01 10:36:35 +0800

    json

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+// 合并为想要的json文件
+const menus = [{
+	id: '1',
+	name: 'preferential',
+	desc: '超值特惠',
+	items: {},
+}, {
+	id: '2',
+	name: 'korea',
+	desc: '韩国节',
+	items: {},
+}];
+const config = {
+	inputDir: '/Users/leaves/Documents/workspace3/activity/static/json',
+	outDir: '/Users/leaves/Documents/workspace3/activity/static/json'
+};
+
+const newMenus = menus.map((item)=>{
+	try{
+		const preferential = fs.readFileSync(`${config.inputDir}/${item.name}.json`, 'utf-8');
+		return {
+			...item,
+			items: preferential
+		}
+	} catch(err){
+		console.log('写文件出错', err);
+	}
+});
+
+try {
+	fs.writeFileSync(`${config.outDir}/app.json`, JSON.stringify(newMenus));
+} catch(err){
+	console.log('写文件出错', err);
+}
